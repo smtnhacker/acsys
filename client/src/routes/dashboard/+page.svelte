19c75a536f35a81aa20f0722e8cd49980f654736
Arukuen <sarmientoalquen@gmail.com>
2023-12-10 23:31:32 +0800

    feat: design dashboard

--- client/src/routes/dashboard/+page.svelte
+++ client/src/routes/dashboard/+page.svelte
@@ -1,4 +1,33 @@
-<div>
-    Dashboard
-    <a href="/problems">Go to Problem List</a>
+<div class='flex h-screen text-white'>
+    <div class='flex flex-col basis-5/6 gap-10 bg-slate-800 px-10 py-6'>
+        <div class='flex gap-10'>
+            <h1 class='text-3xl font-bold'>ACSys</h1>
+            <h1 class='text-3xl font-light'>Dashboard</h1>
+        </div>
+
+        <div class='bg-emerald-300 text-slate-800 rounded-3xl p-10'>
+            <h2 class='text-3xl font-bold'>Welcome back,</h2>
+            <h2 class='text-3xl'>Wilson Tan</h2>
+        </div>
+
+        <h1 class='text-3xl font-light'>Stay Updated with your Students!</h1>
+        
+        <div class='flex justify-between gap-6 h-2/6'>
+            <a class='flex justify-center items-center w-full bg-cyan-600 rounded-3xl' href="/problems">
+                <h3 class="text-slate-800 font-bold text-3xl">Go to Problem List</h3>
+            </a>
+            <div class='w-full bg-cyan-600 rounded-3xl'>
+                
+            </div>
+        </div>
+    </div>
+
+    <div class='flex flex-col basis-1/6 bg-cyan-800 items-center justify-around'>
+        <div class='text-center'>
+            <div class='w-28 h-28 bg-gray-300 rounded-full'></div>
+            <h2 class='text-xl font-bold'>Wilson Tan</h2>
+            <h3>Teacher</h3>
+        </div>
+        <button class='px-4 py-2 bg-emerald-300 text-slate-800 rounded-3xl text-xl font-semibold'>Logout</button>
+    </div>
 </div>
