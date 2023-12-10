5e8e32ae86d0ce07d837c89e9040a4957f39a267
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2023-12-10 22:55:33 +0800

    add css

--- client/src/routes/problems/ListEntry.svelte
+++ client/src/routes/problems/ListEntry.svelte
@@ -1,14 +1,26 @@
 <script>
     export let name;
     export let classID;
+    export let status;
     // export let code;
     export let isEditable;
 </script>
   
-<div>
+<div class="px-7 py-4 bg-gray-100 mx-10 my-3 rounded-md">
 {#if isEditable}
-    <div>{name} | Class {classID} <button>Edit</button></div>
+    <div class="flex p-2 w-5/6 text-lg">
+        <div class="flex-none w-1/4">
+            {#if status == 'Incomplete'}
+                <span class="text-gray-300 px-8 py-1 bg-emerald-700 rounded-xl">{status}</span>
+            {:else}
+                <span class="text-gray-300 px-8 py-1 bg-emerald-400 rounded-xl">{status}</span>
+            {/if}
+        </div>
+
+        <span class="text-slate-800 flex-none w-1/2">{name}</span>
+        <span class="text-slate-800 flex-none w-1/4">{classID}</span>
+    </div>
 {:else}
-    <div>{name} | Class {classID}</div>
+    <div>{status} | {name} | Class {classID}</div>
 {/if}
 </div>
--- client/src/routes/problems/TeacherProblemList.svelte
+++ client/src/routes/problems/TeacherProblemList.svelte
@@ -3,21 +3,25 @@
   
     const SAMPLE = [
       {
+        status: "Incomplete",
         class: "A",
         name: "Simple Operators",
         code: "simple-op",
       },
       {
+        status: "Complete",
         class: "A",
         name: "Conditionals",
         code: "if-else",
       },
       {
+        status: "Incomplete",
         class: "B",
         name: "Peeta and Binary Search",
         code: "bsearch",
       },
       {
+        status: "Complete",
         class: "C",
         name: "Tesseract and the Infinite Tsukuyomi",
         code: "tesseract",
@@ -35,23 +39,36 @@
   </script>
   
   <div>
-    <div>
-      <h1>ACSys | Problem List</h1>
+    <div class="block">
+      <h1 class="text-gray-300 text-5xl p-10">
+        <span class="px-5 font-bold">ACSys</span>
+        <span class="px-3">Problem List</span>
+      </h1>
     </div>
-    <div>
-      Class:{" "}
-      <input
-        type="text"
-        name="class"
-        placeholder="Class ID"
-        bind:value={classID}
-      />
-      <a href="/problems/new">New Problem</a>
+    <div class="flex">
+      <div class="p-5">
+        <span class="text-emerald-300">Class{" "}</span>
+        <input
+          type="text"
+          name="class"
+          placeholder="Class ID"
+          bind:value={classID}
+        />
+      </div>
+      <div class="my-5 px-3 rounded-xl bg-emerald-400">
+        <a href="/problems/new">Add Problem</a>
+      </div>
+    </div>
+    <div class="flex py-2 px-5 text-xl">
+      <span class="text-gray-300 flex-none w-1/4">Status</span>
+      <span class="text-gray-300 flex-none w-1/3">Name</span>
+      <span class="text-gray-300 flex-none w-1/4">Code</span>
     </div>
     <div>
       {#each problems as problem (problem.code)}
         <div>
           <ListEntry
+            status={problem.status}
             name={problem.name}
             classID={problem.class}
             code={problem.code}
