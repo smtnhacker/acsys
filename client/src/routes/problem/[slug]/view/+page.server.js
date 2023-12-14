72613d3cd3214d5f9445fc68336097f9262cee0a
smtnhacker <themikosurara@gmail.com>
2023-12-14 23:22:02 +0800

    fix: use mocks when error

--- client/src/routes/problem/[slug]/view/+page.server.js
+++ client/src/routes/problem/[slug]/view/+page.server.js
@@ -2,8 +2,18 @@ export async function load({ params }) {
     const response = await fetch(`http://127.0.0.1:8000/problem/${params.slug}`);
 
     if (!response.ok) {
-        const error = Error('Failed to fetch details from the server');
-        throw error;
+        // const error = Error('Failed to fetch details from the server');
+        // throw error;
+        console.error("No data found. Using sample");
+        return {
+            details: {
+                    title: "Dani and the Mock",
+                    code: "mock",
+                    class_code: "A",
+                    statement: "# Note \n\n ## This is only for the _mock_",
+                    status: "Incomplete"
+            }
+        }
     }
 
     const data = await response.json();
--- client/src/routes/problems/+page.server.js
+++ client/src/routes/problems/+page.server.js
@@ -2,11 +2,23 @@ export async function load() {
     const response = await fetch('http://127.0.0.1:8000/problems');
 
     if (!response.ok) {
-        const error = new Error('Failed to fetch data from the server');
-        throw error;
+        // const error = new Error('Failed to fetch data from the server');
+        console.error("No data found. Using sample");
+        return {
+            problems: [
+                {
+                    title: "Dani and the Mock",
+                    code: "mock",
+                    class_code: "A",
+                    statement: "# Note \n\n ## This is only for the _mock_",
+                    status: "Incomplete"
+                }
+            ]
+        }
     }
 
     const data = await response.json();
+    console.log(data)
     return {
         problems: data
     }
